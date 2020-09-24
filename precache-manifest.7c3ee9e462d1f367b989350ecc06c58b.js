679f1500e9d40c257a4d54af6ea307751a265ea1
darkness8129 <yaroslaw460@gmail.com>
2020-09-24 20:41:07 +0300

    Updates

---
+++ precache-manifest.7c3ee9e462d1f367b989350ecc06c58b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "990be67102f7c3b570293a1f276c9d67",
+    "url": "/test-task-calendar/index.html"
+  },
+  {
+    "revision": "0166ebf22343d4c11284",
+    "url": "/test-task-calendar/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "10a5655e879dcbce2443",
+    "url": "/test-task-calendar/static/css/main.22c104b8.chunk.css"
+  },
+  {
+    "revision": "0166ebf22343d4c11284",
+    "url": "/test-task-calendar/static/js/2.471cff2a.chunk.js"
+  },
+  {
+    "revision": "2f71fbc01fec0c62d6e32185bc021541",
+    "url": "/test-task-calendar/static/js/2.471cff2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10a5655e879dcbce2443",
+    "url": "/test-task-calendar/static/js/main.469880ac.chunk.js"
+  },
+  {
+    "revision": "b1a0a7d86a6ca2aebecc",
+    "url": "/test-task-calendar/static/js/runtime-main.0fcbccfc.js"
+  }
+]);
